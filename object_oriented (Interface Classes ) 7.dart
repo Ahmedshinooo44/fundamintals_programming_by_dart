e005a81f02f48810d3448cf9ba22654a79984d1b
ahmedshinooo44 <ahmedshinooo44@gmail.com>
2024-07-06 04:15:28 +0300

    Interface Classes

---
+++ object_oriented (Interface Classes ) 7.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: unused_local_variable
+
+void main() {
+  var human1= Student();
+  }
+  //Abstract Methods and Abstract Classes
+  abstract class Human{
+  late String name ;
+  //Abstract Methods
+  void eat();
+  }
+  
+  
+  class Student extends Human {
+  void eat() {
+    print("Student is eating Now");
+    
+    
+	    }
+    }
---
+++ object_oriented (Interface Classes ) 8.dart
@@ -0,0 +1,19 @@
+void main() {
+// ignore: unused_local_variable
+  var student1 = Student();
+}
+
+class Human {
+  Human() {
+    print("Human Default Constructor");
+  }
+  void braath() {
+//code
+  }
+}
+
+class Student extends Human {
+  void braath() {
+    print("Student is braathing Now");
+  }
+}
